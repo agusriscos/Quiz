067c3f11e659a15a4872b15385501b5af2bf924b
Agustín M. de los Riscos Mayorga <agusriscos@gmail.com>
2016-05-13 12:49:37 +0200

    Tema 13:Flash.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -58,16 +58,19 @@ exports.new = function(req, res, next) {
 };
 
 // POST /quizzes/create
+// POST /quizzes/create
 exports.create = function(req, res, next) {
   var quiz = models.Quiz.build({ question: req.body.quiz.question, 
   	                             answer:   req.body.quiz.answer} );
 
-// guarda en DB los campos pregunta y respuesta de quiz
+  // guarda en DB los campos pregunta y respuesta de quiz
   quiz.save({fields: ["question", "answer"]})
   	.then(function(quiz) {
+		req.flash('success', 'Quiz creado con éxito.');
     	res.redirect('/quizzes');  // res.redirect: Redirección HTTP a lista de preguntas
     })
     .catch(function(error) {
+		req.flash('error', 'Error al crear un Quiz: '+error.message);
 		next(error);
 	});  
 };
