92293fbca411717eb584ff134994d166be74f2d2
Agustín M. de los Riscos Mayorga <agusriscos@gmail.com>
2016-05-23 16:19:50 +0200

    Tema25: Roles y Permisos.AuthorId arreglado

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -94,14 +94,18 @@ exports.new = function(req, res, next) {
 
 // POST /quizzes/create
 exports.create = function(req, res, next) {
+
+  var authorId = req.session.user && req.session.user.id || 0;
+
   var quiz = models.Quiz.build({ question: req.body.quiz.question, 
-  	                             answer:   req.body.quiz.answer} );
+                                 answer:   req.body.quiz.answer,
+                                 AuthorId: authorId } );
 
   // guarda en DB los campos pregunta y respuesta de quiz
-  quiz.save({fields: ["question", "answer"]})
-  	.then(function(quiz) {
-		req.flash('success', 'Quiz creado con éxito.');
-    	res.redirect('/quizzes');  // res.redirect: Redirección HTTP a lista de preguntas
+  quiz.save({fields: ["question", "answer", "AuthorId"]})
+    .then(function(quiz) {
+      req.flash('success', 'Quiz creado con éxito.');
+      res.redirect('/quizzes');  // res.redirect: Redirección HTTP a lista de preguntas
     })
     .catch(Sequelize.ValidationError, function(error) {
 
@@ -113,9 +117,9 @@ exports.create = function(req, res, next) {
       res.render('quizzes/new', {quiz: quiz});
     })
     .catch(function(error) {
-		req.flash('error', 'Error al crear un Quiz: '+error.message);
-		next(error);
-	});  
+      req.flash('error', 'Error al crear un Quiz: '+error.message);
+      next(error);
+  });  
 };
 // GET /quizzes/:id/edit
 exports.edit = function(req, res, next) {
